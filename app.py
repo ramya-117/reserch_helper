78ac7f3660357be86a3a3b3a0312bb260d78f740
Gomathi <mahadevsputhri@gmail.com>
2026-03-02 22:55:50 +0530

    Fix app.py to run Flask app correctly

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def status(job_id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
